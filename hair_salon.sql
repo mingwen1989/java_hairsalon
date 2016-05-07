5d211b7d635a0ae7140fd099b19327fc2c05461e
themingfu <mwen82@gmail.com>
2016-05-06 19:58:21 -0700

    Backup Database

--- hair_salon.sql
+++ hair_salon.sql
@@ -34,18 +34,32 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: doctors; Type: TABLE; Schema: public; Owner: MW
+-- Name: clients; Type: TABLE; Schema: public; Owner: MW
 --
 
-CREATE TABLE doctors (
+CREATE TABLE clients (
     id integer NOT NULL,
     first_name character varying,
     last_name character varying,
-    specialty character varying
+    birthdate character varying,
+    stylistid integer
 );
 
 
-ALTER TABLE doctors OWNER TO "MW";
+ALTER TABLE clients OWNER TO "MW";
+
+--
+-- Name: stylists; Type: TABLE; Schema: public; Owner: MW
+--
+
+CREATE TABLE stylists (
+    id integer NOT NULL,
+    first_name character varying,
+    last_name character varying
+);
+
+
+ALTER TABLE stylists OWNER TO "MW";
 
 --
 -- Name: doctors_id_seq; Type: SEQUENCE; Schema: public; Owner: MW
@@ -65,23 +79,8 @@ ALTER TABLE doctors_id_seq OWNER TO "MW";
 -- Name: doctors_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: MW
 --
 
-ALTER SEQUENCE doctors_id_seq OWNED BY doctors.id;
+ALTER SEQUENCE doctors_id_seq OWNED BY stylists.id;
 
-
---
--- Name: patients; Type: TABLE; Schema: public; Owner: MW
---
-
-CREATE TABLE patients (
-    id integer NOT NULL,
-    first_name character varying,
-    last_name character varying,
-    birthdate character varying,
-    doctorid integer
-);
-
-
-ALTER TABLE patients OWNER TO "MW";
 
 --
 -- Name: patients_id_seq; Type: SEQUENCE; Schema: public; Owner: MW
@@ -101,7 +100,7 @@ ALTER TABLE patients_id_seq OWNER TO "MW";
 -- Name: patients_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: MW
 --
 
-ALTER SEQUENCE patients_id_seq OWNED BY patients.id;
+ALTER SEQUENCE patients_id_seq OWNED BY clients.id;
 
 
 --
@@ -141,14 +140,7 @@ ALTER SEQUENCE specialties_id_seq OWNED BY specialties.id;
 -- Name: id; Type: DEFAULT; Schema: public; Owner: MW
 --
 
-ALTER TABLE ONLY doctors ALTER COLUMN id SET DEFAULT nextval('doctors_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: MW
---
-
-ALTER TABLE ONLY patients ALTER COLUMN id SET DEFAULT nextval('patients_id_seq'::regclass);
+ALTER TABLE ONLY clients ALTER COLUMN id SET DEFAULT nextval('patients_id_seq'::regclass);
 
 
 --
@@ -159,17 +151,23 @@ ALTER TABLE ONLY specialties ALTER COLUMN id SET DEFAULT nextval('specialties_id
 
 
 --
--- Data for Name: doctors; Type: TABLE DATA; Schema: public; Owner: MW
+-- Name: id; Type: DEFAULT; Schema: public; Owner: MW
 --
 
-COPY doctors (id, first_name, last_name, specialty) FROM stdin;
-1	Alex	Smith	Pediatrics
-2	Lucas	Jones	Dermatology
-3	Lisa	Arthur	Optometry
-4	Mark	Scott	Neurology
-5	Thomas 	Aaron	Dermatology
-7	Paul 	Fitzgerald	\N
-8	Scott	Fitzgerald	\N
+ALTER TABLE ONLY stylists ALTER COLUMN id SET DEFAULT nextval('doctors_id_seq'::regclass);
+
+
+--
+-- Data for Name: clients; Type: TABLE DATA; Schema: public; Owner: MW
+--
+
+COPY clients (id, first_name, last_name, birthdate, stylistid) FROM stdin;
+1	John	Doe	1985-05-06	1
+4	Margret	Ann	1974-08-12	3
+2	Randel	Cobb	1995-03-21	2
+3	Mourice	Jean-Lucas	1987-07-05	1
+5	Darren	Marks	1979-09-09	5
+10	Margaret	Thatcher	1954-08-09	5
 \.
 
 
@@ -177,27 +175,14 @@ COPY doctors (id, first_name, last_name, specialty) FROM stdin;
 -- Name: doctors_id_seq; Type: SEQUENCE SET; Schema: public; Owner: MW
 --
 
-SELECT pg_catalog.setval('doctors_id_seq', 8, true);
-
-
---
--- Data for Name: patients; Type: TABLE DATA; Schema: public; Owner: MW
---
-
-COPY patients (id, first_name, last_name, birthdate, doctorid) FROM stdin;
-1	John	Doe	1985-05-06	1
-4	Margret	Ann	1974-08-12	3
-2	Randel	Cobb	1995-03-21	2
-3	Mourice	Jean-Lucas	1987-07-05	1
-5	Darren	Marks	1979-09-09	5
-\.
+SELECT pg_catalog.setval('doctors_id_seq', 10, true);
 
 
 --
 -- Name: patients_id_seq; Type: SEQUENCE SET; Schema: public; Owner: MW
 --
 
-SELECT pg_catalog.setval('patients_id_seq', 5, true);
+SELECT pg_catalog.setval('patients_id_seq', 10, true);
 
 
 --
@@ -223,10 +208,27 @@ SELECT pg_catalog.setval('specialties_id_seq', 7, true);
 
 
 --
+-- Data for Name: stylists; Type: TABLE DATA; Schema: public; Owner: MW
+--
+
+COPY stylists (id, first_name, last_name) FROM stdin;
+1	Alex	Smith
+2	Lucas	Jones
+3	Lisa	Arthur
+4	Mark	Scott
+5	Thomas 	Aaron
+7	Paul 	Fitzgerald
+8	Scott	Fitzgerald
+9	Douglas	MacArthur
+10	Robert	Fitzgerald
+\.
+
+
+--
 -- Name: doctors_pkey; Type: CONSTRAINT; Schema: public; Owner: MW
 --
 
-ALTER TABLE ONLY doctors
+ALTER TABLE ONLY stylists
     ADD CONSTRAINT doctors_pkey PRIMARY KEY (id);
 
 
@@ -234,7 +236,7 @@ ALTER TABLE ONLY doctors
 -- Name: patients_pkey; Type: CONSTRAINT; Schema: public; Owner: MW
 --
 
-ALTER TABLE ONLY patients
+ALTER TABLE ONLY clients
     ADD CONSTRAINT patients_pkey PRIMARY KEY (id);
 
 
